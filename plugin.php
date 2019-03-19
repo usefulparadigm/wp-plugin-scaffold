d1ce015aba9465a94ab5add1bdca6036ec045d99
Sukjoon Kim <sjoonk@gmail.com>
2019-03-19 16:36:50 +0900

    update plugin

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: PluginScaffold
- * Plugin URI:  
- * Description: 
+ * Plugin URI:  PLUGIN SITE HERE
+ * Description: PLUGIN DESCRIPTION HERE
  * Version:     0.1.0
  * Author:      Useful Paradigm
  * Author URI:  https://usefulparadigm.com
@@ -12,6 +12,9 @@
  * @package PluginScaffold
  */
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 // Some global constants
 define( 'PLUGIN_SCAFFOLD_VERSION', '0.1.0' );
 define( 'PLUGIN_SCAFFOLD_URL', plugin_dir_url( __FILE__ ) );
@@ -20,6 +23,7 @@ define( 'PLUGIN_SCAFFOLD_INC', PLUGIN_SCAFFOLD_PATH . 'includes/' );
 
 // Include files
 require_once PLUGIN_SCAFFOLD_INC . 'functions.php';
+// require_once PLUGIN_SCAFFOLD_INC . 'settings.php';
 
 // activation/deactivation
 register_activation_hook( __FILE__, '\PluginScaffold\activate' );
@@ -27,6 +31,7 @@ register_deactivation_hook( __FILE__, '\PluginScaffold\deactivate' );
 
 // Bootstrap
 PluginScaffold\setup();
+// PluginScaffold\Settings\setup();
 
 // Require Composer autoloader if it exists.
 if ( file_exists( PLUGIN_SCAFFOLD_PATH . '/vendor/autoload.php' ) ) {
